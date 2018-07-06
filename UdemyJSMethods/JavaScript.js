f1cc19e7d1f05470955af40addd3dc61d1dfb8df
Amy Thompson <amythompson9@gmail.com>
2018-07-05 20:23:28 -0400

    Understanding THIS

--- UdemyJSMethods/JavaScript.js
+++ UdemyJSMethods/JavaScript.js
@@ -1,1 +1,21 @@
-﻿
+﻿var comments = {};
+
+comments.data = ["Good Job!", "Bye", "Lame..."]
+
+function print(arr) {
+    arr.forEach(function (el) {
+        console.log(el);
+    });
+}
+
+print(comments.data);
+
+//OR!
+
+comments.print = function() {
+    this.data.forEach(function (el) {
+        console.log(el);
+    });
+}
+
+//the word this refers to the object comments
